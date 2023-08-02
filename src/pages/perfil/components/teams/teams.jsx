70bd4876b093d09ecc1641cf69aa4c5277ee4ecc
dandorock <daniel-rl-98@hotmail.com>
2023-08-02 16:14:23 -0300

    change

--- src/pages/perfil/components/teams/teams.jsx
+++ src/pages/perfil/components/teams/teams.jsx
@@ -17,8 +17,6 @@ export default function Teams(props) {
     "Warrios three",
   ];
 
-  let newArray = [];
-
   const [character, setCharacter] = useState([]);
   const [load, setLoad] = useState(true);
 
